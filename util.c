6ec3f50ec3c96a826651c24b19cf47e9a31e26f8
Max Rees <maxcrees@me.com>
2020-12-13 04:04:52 -0500

    Split out two subroutines from pull_pathname
    Now we can also write back to the tracee using tx_data(push=1). In the
    future if we need to rewrite only a pathname in a syscall we could also
    mutate pull_pathname into tx_pathname(push=).
    
    Use check_pathname to ensure PATH_MAX paths from tx_data() don't
    overflow. pull_pathname automatically calls this for you.

--- util.c
+++ util.c
@@ -1,13 +1,11 @@
 /* SPDX-License-Identifier: NCSA */
 #include <errno.h>   /* errno */
 #include <fcntl.h>   /* AT_* */
-#include <limits.h>  /* PATH_MAX */
 #include <stdio.h>   /* snprintf */
 #include <string.h>  /* strnlen */
-#include <sys/uio.h> /* process_vm_readv */
-#include <unistd.h>  /* chdir */
 
 #include "comproot.h"
+#include "util.h"
 
 int chdir_to_fd(pid_t pid, int fd, char *procpath) {
 	int rc;
@@ -35,32 +33,57 @@ int chdir_to_fd(pid_t pid, int fd, char *procpath) {
 	return 0;
 }
 
-int pull_pathname(int notifyfd, struct seccomp_notif *req, int argno, char pathname[PATH_MAX]) {
+int tx_data(int notifyfd, struct seccomp_notif *req, PROC_VM_BUFS, int push) {
 	int rc;
-	struct iovec liov[] = {{pathname, PATH_MAX+1}};
-	struct iovec riov[] = {{(void *)req->data.args[argno], PATH_MAX+1}};
+	ssize_t (*func)(pid_t pid, PROC_VM_BUFS, unsigned long flags) = 0;
 
-	if (process_vm_readv(req->pid, liov, 1, riov, 1, 0) < 0) {
+	if (push)
+		func = process_vm_writev;
+	else
+		func = process_vm_readv;
+
+	if ((rc = func(req->pid, liov, nl, riov, nr, 0)) == -1) {
 #ifndef NDEBUG
-		pwarn(req->pid, "process_vm_readv");
+		pwarn(req->pid, "tx_data");
 #endif
 		errno = EIO;
-		return -1;
+		goto out;
 	}
 	if (seccomp_notify_id_valid(notifyfd, req->id)) {
 #ifndef NDEBUG
 		pwarnx(req->pid, "died during trace!");
 #endif
+		rc = -1;
 		errno = EIO;
-		return -1;
+		goto out;
 	}
+
+out:
+	return rc;
+}
+
+int check_pathname(char pathname[PATH_MAX]) {
+	int rc;
 
 	if ((rc = strnlen(pathname, PATH_MAX+1)) == PATH_MAX+1) {
 #ifndef NDEBUG
-		pwarnx(req->pid, "pathname is bigger than PATH_MAX");
+		warnx("pathname is bigger than PATH_MAX");
+#endif
 		errno = ENAMETOOLONG;
 		return -1;
-#endif
 	}
+	return rc;
+}
+
+int pull_pathname(int notifyfd, struct seccomp_notif *req, int argno, char pathname[PATH_MAX]) {
+	int rc;
+	struct iovec liov[] = {{pathname, PATH_MAX+1}};
+	struct iovec riov[] = {{(void *)req->data.args[argno], PATH_MAX+1}};
+
+	if ((rc = tx_data(notifyfd, req, liov, 1, riov, 1, 0)) == -1)
+		goto out;
+
+	rc = check_pathname(pathname);
+out:
 	return rc;
 }
--- util.h
+++ util.h
@@ -1,8 +1,15 @@
 #pragma once
 /* SPDX-License-Identifier: NCSA */
-#include <limits.h> /* PATH_MAX */
+#include <limits.h>  /* PATH_MAX */
+#include <sys/uio.h> /* struct iovec */
+#include <unistd.h>  /* pid_t */
 
 #include "comproot.h"
 
 int chdir_to_fd(pid_t pid, int fd, char *procpath);
+
+#define PROC_VM_BUFS const struct iovec liov[], unsigned long nl, const struct iovec riov[], unsigned long nr
+int tx_data(int notifyfd, struct seccomp_notif *req, PROC_VM_BUFS, int push);
+
+int check_pathname(char pathname[PATH_MAX]);
 int pull_pathname(int notifyfd, struct seccomp_notif *req, int argno, char pathname[PATH_MAX]);
